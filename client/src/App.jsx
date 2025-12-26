3443b10ce4acbe4f46c26b6fb8ce89bd7edb50b2
abdulmajid-1 <imabddulmajid2000@gmail.com>
2025-12-26 23:45:14 +0500

    adding folder structure and installing dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div class="flex items-center justify-center h-screen bg-gray-950">
+      <h1 class="text-3xl font-bold underline text-white">
+        Learning Websockets
+      </h1>
+    </div>
+  );
+};
+
+export default App;
