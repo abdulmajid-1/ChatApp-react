4a3cb78d93580a87562556d3d527d9abbd628ff5
abdulmajid-1 <imabddulmajid2000@gmail.com>
2026-01-04 18:38:54 +0500

    Profile Page is done for static data

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -16,9 +16,13 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
         className="flex items-center gap-3 py-3 mx-4 
         border-b border-stone-500"
       >
-        <img src={assets.profile_martin} alt="" className="w-8 rounded-full" />
+        <img
+          src={selectedUser?.profilePic || assets.profile_martin}
+          alt=""
+          className="w-8 rounded-full"
+        />
         <p className="flex-1 text-lg text-white flex items-center gap-2">
-          Martin Johnson
+          {selectedUser?.fullName}
           <span className="w-2 h-2 rounded-full bg-green-500"></span>
         </p>
         <img
