e328478b2a05c5cd6a9b82932dbee70797d8c5c3
abdulmajid-1 <imabddulmajid2000@gmail.com>
2026-03-31 23:44:43 +0500

    Setup AuthContext and started working on backend integration

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import assets from "../assets/assets";
 import { useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const LoginPage = () => {
   const [currState, setCurrState] = useState("Sign up");
@@ -10,6 +11,8 @@ const LoginPage = () => {
   const [bio, setBio] = useState("");
   const [isDataSubmitted, setIsDataSubmitted] = useState(false);
 
+  const { login } = useContext(AuthContext);
+
   const onSubmitHandler = () => {
     event.preventDefault();
 
@@ -17,6 +20,12 @@ const LoginPage = () => {
       setIsDataSubmitted(true);
       return;
     }
+    login(currState === "Sign up" ? "signup" : "login", {
+      fullName,
+      email,
+      password,
+      bio,
+    });
   };
 
   return (
